2d8b70ef14a2a355936d4e0f3d87e67e0a232d86
Adamuabbas <basboyzamaniey@gmail.com>
2024-10-20 10:13:11 +0100

    Production ready

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
   
     app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     );
   } else {
     const __dirname = path.resolve();
